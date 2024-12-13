85867a9c44eef11a71c3f811706ef79031abfe71
AlbaCastilla <castillamacki@gmail.com>
2024-12-13 11:14:22 +0100

    Parte visual con colores del figma, perfilUsuario

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,13 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import PerfilUsuario from './lib/PerfilUsuario.svelte'
+  import RegistroUsuario from './lib/RegistroUsuario.svelte';
 </script>
 
 
 
 <main>
   <div class="pag">
-    <PerfilUsuario />
+    <PerfilUsuario/>
 </div>
 </main>
